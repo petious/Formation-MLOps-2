f3b7c57144e8b6c1e66a8ccbab8f9491b34279b4
ostaron <oceane.staron@gmail.com>
2023-12-14 14:49:11 +0000

    add timestamp to model filename

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 
 import joblib
 import pandas as pd
@@ -18,7 +19,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
-    joblib.dump(model, os.path.join(model_registry_folder, 'model.joblib'))
+    joblib.dump(model, os.path.join(model_registry_folder, datetime.now().strftime("%Y%m%d-%H%M%S") + '.joblib'))
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
